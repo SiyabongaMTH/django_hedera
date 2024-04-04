f891b51a917fbe03ab05f6ca124f8bac5b7f1313
siyabongamthombeni <siyabongamthombeni82@gmail.com>
2024-04-04 12:49:39 +0200

    Django project

---
+++ core/project/urls.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+from django.contrib import admin
+from django.urls import path
+
+from core.project.settings import ADMIN_PATH
+
+urlpatterns = [
+    path(f'{ADMIN_PATH}/', admin.site.urls),
+]
